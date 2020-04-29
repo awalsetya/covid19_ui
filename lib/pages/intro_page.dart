f5d8734872e111fb657d8de03315dbb18cc57d76
awal <awalsetyanto.1993@gmail.com>
2020-04-29 22:24:23 +0700

    add image

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -24,45 +24,84 @@ class _IntroPageState extends State<IntroPage> {
         )),
         child: Stack(
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(15, 50, 15, 0),
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Image.asset(
-                  "assets/images/covid19.png",
-                ),
+            buildHeader(),
+            Image.asset(
+                "assets/images/covid19.png",
               ),
-            ),
-            Positioned(
-              bottom: 50,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "Corona virus disease (Covid19)",
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      "is an infectianus disease caused by a new virus",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                        height: 1.5,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-              ),
-            )
+            _buildFooter(context)
           ],
         ),
       ),
     );
+  }
+
+  Padding buildHeader() {
+    return Padding(
+            padding: const EdgeInsets.fromLTRB(15, 50, 15, 0),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Image.asset(
+                "assets/images/covid19.png",
+              ),
+            ),
+          );
+  }
+
+  Widget _buildFooter(BuildContext context) {
+    return Positioned(
+            bottom: 50,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Corona virus disease (Covid19)",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "is an infectianus disease caused by a new virus",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      height: 1.5,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 25,),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color:Colors.black26,
+                          offset: Offset(1, 1),
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                        )
+                      ]
+                    ),
+                    width: MediaQuery.of(context).size.width *.85,
+                    height: 60,
+                    child: Center(
+                      child: Text("GET STARTED",
+                      style: TextStyle(
+                        color:AppColors.mainColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
   }
 }
